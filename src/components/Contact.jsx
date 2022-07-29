07c0525cba7a40a4093768b0ccdc1f4ba825c368
antran1245 <antran1245@gmail.com>
2022-07-29 13:22:38 -0700

    created my messagebox

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,33 +27,33 @@ export default function Contact (props) {
     }
     return(
         <section id="contact" ref={e => props.refer.current[3] = e}>
-            <MessageBox>
-                <Form onSubmit={submitHandle}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Name <Form.Text className="text-muted">(Optional)</Form.Text></Form.Label>
-                        <Form.Control type="text" placeholder="Enter your name" onChange={(e) => setForm({...form, name:e.target.value})}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Email Address<span className="text-danger">*</span></Form.Label>
-                        {error.email && <p className="text-danger">Email is required</p>}
-                        <Form.Control type="text" placeholder="Enter email" onChange={(e) => setForm({...form, email:e.target.value})}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Subject <Form.Text className="text-muted">(Optional)</Form.Text></Form.Label>
-                        <Form.Control type="text" placeholder="Enter subject" onChange={(e) => setForm({...form, subject:e.target.value})}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Detail<span className="text-danger">*</span></Form.Label>
-                        {error.body && <p className="text-danger">Detail is required</p>}
-                        <Form.Control as={"textarea"} rows={4} onChange={(e) => setForm({...form, body: e.target.value})}/>
-                    </Form.Group>
-                    <Row>
-                        <Col className="d-flex justify-content-end">
-                            <Button variant="primary" type="submit" className="w-50">Send</Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </MessageBox>
-        </section>
+                <MessageBox>
+                    <Form onSubmit={submitHandle}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Name <Form.Text className="text-muted">(Optional)</Form.Text></Form.Label>
+                            <Form.Control type="text" placeholder="Enter your name" onChange={(e) => setForm({...form, name:e.target.value})}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email Address<span className="text-danger">*</span></Form.Label>
+                            {error.email && <p className="text-danger">Email is required</p>}
+                            <Form.Control type="text" placeholder="Enter email" onChange={(e) => setForm({...form, email:e.target.value})}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Subject <Form.Text className="text-muted">(Optional)</Form.Text></Form.Label>
+                            <Form.Control type="text" placeholder="Enter subject" onChange={(e) => setForm({...form, subject:e.target.value})}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Detail<span className="text-danger">*</span></Form.Label>
+                            {error.body && <p className="text-danger">Detail is required</p>}
+                            <Form.Control as={"textarea"} rows={4} onChange={(e) => setForm({...form, body: e.target.value})}/>
+                        </Form.Group>
+                        <Row>
+                            <Col className="d-flex justify-content-end">
+                                <Button variant="primary" type="submit" className="w-50">Send</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </MessageBox>
+            </section>
     );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,26 +4,29 @@ import ToDo from "./projects/ToDo";
 import CrimeReport from "./projects/CrimeReport";
 import Location from './projects/Location';
 import "../sass/project.scss";
+import MessageBox from "../design/MessageBox";
 
 export default function Projects(props) {
 
     return(
         <section id="projects" ref={e => props.refer.current[2] = e}>
             <br />
-            <Row className="project">
-                <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
-                    <JokeBoard />
-                </Col>
-                <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
-                    <ToDo />
-                </Col>
-                <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
-                    <CrimeReport />
-                </Col>
-                <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
-                    <Location />
-                </Col>
-            </Row>
+            <MessageBox>
+                <Row className="project">
+                    <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
+                        <JokeBoard />
+                    </Col>
+                    <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
+                        <ToDo />
+                    </Col>
+                    <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
+                        <CrimeReport />
+                    </Col>
+                    <Col xs={12} md={{span: 6}} lg={4} className="mt-3">
+                        <Location />
+                    </Col>
+                </Row>
+            </MessageBox>
         </section>
     );
 }
