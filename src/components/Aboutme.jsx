c7c62175c8c900d83e906a154ef922b672cc5ace
antran1245 <antran1245@gmail.com>
2022-07-31 21:22:49 -0700

    added icon on to about me

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,6 +1,9 @@
 import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import MessageBox from "../design/MessageBox";
-import resume from "../Resume.pdf";
+import resume from "../An_Tran_Resume.pdf";
 import '../sass/aboutme.scss';
 
 export default function Aboutme(props) {
@@ -26,9 +29,14 @@ export default function Aboutme(props) {
                     <Col>
                         <b>
                             <a href="https://github.com/antran1245">
+                                <FontAwesomeIcon icon={faGithub} />
                                 Github</a> | &nbsp;
-                            <a href="https://www.linkedin.com/in/an-tran1245/">Linkedin</a> | &nbsp;
-                            <a href={resume} download>Resume</a>
+                            <a href="https://www.linkedin.com/in/an-tran1245/">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                                Linkedin</a> | &nbsp;
+                            <a href={resume} download>
+                                <FontAwesomeIcon icon={faFilePdf} />
+                                Resume</a>
                         </b>
                     </Col>
                 </Row>
